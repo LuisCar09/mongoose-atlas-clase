a058674ea34ef97bced1999d53d0a2302074a8c9
Luis Carlos <luiscbravo94@gmail.com>
2024-09-21 00:57:42 +0100

    project finished

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const {dbConnection} = require('./config/config')
+const routes = require('./routes/index')
+
 dotenv.config()
+
 const app = express()
 const PORT = process.env.PORT ?? 4000 
 
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+
 dbConnection()
 
+app.use('/',routes)
 
 
 app.listen(PORT,() => {
